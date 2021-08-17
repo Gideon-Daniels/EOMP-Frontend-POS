0da07a08b8ecbb493ee892c7bc8352e998d113ff
Dragoonix <gideondaniels.dragoonix@gmail.com>
2021-08-17 16:36:35 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,113 @@
-
-
 /////////////////////////////////////////// Data from API////////////////////////////////////////
 myStorage = window.localStorage;
-urlAuth = "https://enigmatic-sierra-22968.herokuapp.com/auth"
-urlsShowUsers = 'https://enigmatic-sierra-22968.herokuapp.com/show-users/'
-urlsShowProducts = 'https://enigmatic-sierra-22968.herokuapp.com/show-products/'
-urlEditProduct = 'https://enigmatic-sierra-22968.herokuapp.com/edit-product/'//add number next to it
-urlDeleteProduct = 'https://enigmatic-sierra-22968.herokuapp.com/delete-product/' //add number next to it
-urlAddProduct = "https://enigmatic-sierra-22968.herokuapp.com/add-product/"
-urlShowTyp = "https://enigmatic-sierra-22968.herokuapp.com/" // add category
-let username = document.getElementsByName("username").value
-let password = document.getElementsByName("password").value
+urlAuth = "https://enigmatic-sierra-22968.herokuapp.com/auth";
+urlsShowUsers = "https://enigmatic-sierra-22968.herokuapp.com/show-users/";
+urlsShowProducts =
+  "https://enigmatic-sierra-22968.herokuapp.com/show-products/";
+urlEditProduct = "https://enigmatic-sierra-22968.herokuapp.com/edit-product/"; //add number next to it
+urlDeleteProduct =
+  "https://enigmatic-sierra-22968.herokuapp.com/delete-product/"; //add number next to it
+urlAddProduct = "https://enigmatic-sierra-22968.herokuapp.com/add-product/";
+urlShowTyp = "https://enigmatic-sierra-22968.herokuapp.com/"; // add category
 
-function authorization(username,password){
-  fetch(urlAuth, {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({"username": `${username}`, "password": `${password}`})
-  }).then(res => res.json())
-    .then(res => {
-              console.log(res);             
-              console.log(res["access_token"]);
-              console.log(myStorage)
-              myStorage.setItem("jwt-token", res["access_token"]);
-              
-     });
+function validationLogin(){
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  
+  if(username.value.trim() == "" )
+  {
+    username.style.border = "solid 3px red";
+    return false
+  }
+  else if(password.value.trim() == "")
+  {
+    return false
+  }
+  else{
+    authorization();
+    return true
+  }
+  
 }
 
-authorization(username, password);
+function validationRegistration(){
+  let names = document.getElementById("name").value
+  let surname = document.getElementById("surname").value
+  let email = document.getElementById("email").value
+  let regUsername = document.getElementById("reg-username").value
+  let regPassword = document.getElementById("reg-password").value
+  let confPassword = document.getElementById("confirm-password").value
+  if (names.trim() == "" ||
+      surname.trim() == "" ||
+      username.trim() == ""||
+      password.trim() == ""||
+      email.trim() == "" ||
+      regUsername.trim() == ""||
+      regPassword.trim() == ""||
+      confPassword.trim() == ""
+  )
+  {
+    alert("Please full in all inputs")
+    return false
+  }
+  else if(confPassword.trim()!=regPassword.trim()){
+    alert("Confirmed password doesn't match password");
+      return false
+  }
+  else{
+    regUser = {
+      names : names.trim(),
+      surname: surname.trim(),
+      email: email.trim(),
+      password: password.trim(),
+      confPassword:confPassword.trim()
+    }
+    registeredUsers.push(regUser)
+    return  true
+  }
+}
+registeredUsers = []
 
+function authorization() {
+  fetch(urlAuth, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username: "gideon", password: "1234" }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data["access_token"]);
+      console.log(myStorage);
+      myStorage.setItem("jwt-token", data["access_token"]);
+    });
+}
+
+authorization();
 ///////////////////////////////// Products Functionality //////////////////////
 
 let products = []; // Add products to array
 
-function fecthProducts(){
-  fetch( urlsShowProducts, {
-    method: 'get',
+function fecthProducts() {
+  fetch(urlsShowProducts, {
+    method: "get",
     headers: {
-      "Authorization": `jwt ${myStorage.getItem("jwt-token")}`,
-      "Content-Type": "application/json"
+      Authorization: `jwt ${myStorage.getItem("jwt-token")}`,
+      "Content-Type": "application/json",
     },
-})
-.then(res => res.json())
-.then(data => {
-  console.log(data)
-  products = data.data;
-  createdProducts(data.data);
-});
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      products = data.data;
+      createdProducts(data.data);
+    });
 }
 fecthProducts();
 
-function createdProducts(products){
+function createdProducts(products) {
   let productsContainer = document.querySelector(".products-container");
   productsContainer.innerHTML = "";
   products.forEach((product) => {
@@ -72,34 +127,32 @@ function createdProducts(products){
   });
 }
 //////////////////// delete products functionality///////////
-function deleteProduct(id){
-    console.log(products)
+function deleteProduct(id) {
+  console.log(products);
 
-    console.log(id) 
-    let index=0;
-    let deletedProducts = products.find( item => {
-      console.log("item",item[0]);
-      if( item[0] == id){
-        products.splice(index,1);
-       
-        createdProducts(products);
-      }     
-      else{
-        index += 1;
-      }
-    });
-  
+  console.log(id);
+  let index = 0;
+  let deletedProducts = products.find((item) => {
+    console.log("item", item[0]);
+    if (item[0] == id) {
+      products.splice(index, 1);
+
+      createdProducts(products);
+    } else {
+      index += 1;
+    }
+  });
 }
 
 //////////Update Products Functionality////
 
 let updateProducts = document.querySelector("#update-product");
-updateProducts.innerHTML="";
+updateProducts.innerHTML = "";
 
-function updateProduct(id){
-    product = products[id-1];
-    updateProducts.innerHTML =`                        
-    <form onsubmit="event.preventDefault()" id="update-products" class="forms" method="POST">
+function updateProduct(id) {
+  product = products[id - 1];
+  updateProducts.innerHTML = `                        
+    <form onsubmit="event.preventDefault();" id="update-products" class="forms" method="POST">
         <h2 class="heading">Update Product</h2>
         <input class="input" type="text" name="update-product-name" id="update-product-name" required placeholder="product name" value="${product[1]}">
         <input class="input" type="text" name="update-product-description" id="update-product-description" required placeholder="product description" value="${product[2]}">
@@ -108,8 +161,10 @@ function updateProduct(id){
         <input type="file" id="image" name="update-image" accept="image/png">
         <input class = "button" type="submit" name="submit" type="submit" value="confirm">
 </form>
-    `;  
-     productsForm.style.backgroundColor = "darksalmon"
+    `;
+  productsForm.style.backgroundColor = "aqua";
+
+
 }
 
 // function updateProducts(){
@@ -126,27 +181,26 @@ function updateProduct(id){
 //       // "description": `${}`
 //     }
 //   })
-  
+
 // }
 
-
 //////////////////////Filters///////////////////
-function searchForProducts(){
+function searchForProducts() {
   let searchItem = document.querySelector("#searchItem").value;
   console.log(searchItem);
- 
-  let i=0;
-  let searchedProducts = products.data.filter((product) =>{
-    products[1].toLowerCase().startsWith(searchItem.toLowerCase())
-    i+=1;
+
+  let i = 0;
+  let searchedProducts = products.data.filter((product) => {
+    products[1].toLowerCase().startsWith(searchItem.toLowerCase());
+    i += 1;
   });
-  console.log(products.data[i][1]) 
+  console.log(products.data[i][1]);
   console.log(searchedProducts);
-  createdProducts(searchedProducts)
+  createdProducts(searchedProducts);
 }
 
-function sortName(){
-  let sortedProducts = products.sort((a,b) => {
+function sortName() {
+  let sortedProducts = products.sort((a, b) => {
     if (a[1] < b[1]) return -1;
     if (a[1] > b[1]) return 1;
     return 0;
@@ -156,7 +210,7 @@ function sortName(){
 }
 
 function sortNameDesc() {
-  let sortedProducts = products.sort((a,b) => {
+  let sortedProducts = products.sort((a, b) => {
     if (a[1] < b[1]) return -1;
     if (a[1] > b[1]) return 1;
     return 0;
@@ -164,15 +218,14 @@ function sortNameDesc() {
 
   sortedProducts.reverse();
   createdProducts(sortedProducts);
-
 }
 
-function sortPriceAsc(){
+function sortPriceAsc() {
   let sortedProducts = products.sort((a, b) => a[4] - b[4]);
   createdProducts(sortedProducts);
 }
 
-function sortPriceDesc(){
+function sortPriceDesc() {
   let sortedProducts = products.sort((a, b) => a[4] - b[4]).reverse();
   createdProducts(sortedProducts);
 }
@@ -181,7 +234,7 @@ function sortPriceDesc(){
 productsForm = document.querySelector(".create-container");
 productsForm.innerHTML = "";
 
-function formCreated(){
+function formCreated() {
   productsForm.innerHTML = `
   <form onsubmit="event.preventDefault()" id="create-products" class="forms" method="POST">
     <h2 class="heading">Create Product</h2>
@@ -192,26 +245,25 @@ function formCreated(){
     <input type="file" id="image" name="image" accept="image/png">
     <input class = "button" type="submit" name="submit" type="submit" value="register">
 </form>
-  `
- 
+  `;
+
   return productsForm;
 }
 formCreated();
-
 
 //////////////////////////////// Cart Products Funtionality/////////////////////////////
 let cart = []; // add products to array
 let totalPrice = 0;
 
 //////////////Display Products ///////////////////
-function renderProdctsInCart(cartProducts){
+function renderProdctsInCart(cartProducts) {
   let cartContainer = document.querySelector(".cart-container");
   cartContainer.innerHTML = "";
-  
-  if (cartProducts.length > 0){
-    let i =0;
+
+  if (cartProducts.length > 0) {
+    
     cartProducts.map((cartProduct) => {
-        cartContainer.innerHTML += `
+      cartContainer.innerHTML += `
         <div class="product">
         <div class="card-image">
             <img src="${cartProduct[3]}" alt="${cartProduct[1]}">
@@ -221,89 +273,83 @@ function renderProdctsInCart(cartProducts){
             <button class="delete-product button" onclick="deleteFromCart(${cartProduct[0]})">Delete</button>
         </div>
     </div>
-        `
-        i+=1;
+        `;
+     
     });
 
     ////////////// calcutlates total price of products in cart/////////////
-     totalPrice = cartProducts.reduce((total, item) => total + item[4], 0);
+    totalPrice = cartProducts.reduce((total, item) => total + item[4], 0);
     let cartInfo = document.querySelector(".cart-info");
     cartInfo.innerHTML = `
         <h3 class="total">Total Price : ${totalPrice}</h3>
 
     `;
-    
-    console.log(totalPrice);
 
-  }
-  else{
-    cartContainer.innerHTML =  "<h2> No Items in cart </h2>";
+    console.log(totalPrice);
+  } else {
+    cartContainer.innerHTML = "<h2> No Items in cart </h2>";
   }
 }
 //////////////////////// Adding and Deleting products in Cart Functions/////////////////
-function addToCart(id){
-  let i=0;
-  let product = products.find(item => {
-     if (item[0] == id){
-       return item;
-     }
+function addToCart(id) {
+  let i = 0;
+  let product = products.find((item) => {
+    if (item[0] == id) {
+      return item;
+    }
   });
-  
+
   console.log(product);
   cart.push(product);
   console.log("Items in Cart :", cart);
   renderProdctsInCart(cart);
 }
 
-function deleteFromCart(id){
-  console.log(id) 
+function deleteFromCart(id) {
+  console.log(id);
   let index = 0;
-  let product = cart.find( item => {
-    if (item[0] == id){
-      console.log(index)
-      cart.splice(index,1);
-      totalPrice = totalPrice - item[4]     
-      console.log(cart.length)
+  let product = cart.find((item) => {
+    if (item[0] == id) {
+      console.log(index);
+      cart.splice(index, 1);
+      totalPrice = totalPrice - item[4];
+      console.log(cart.length);
       renderProdctsInCart(cart);
-      
-    }
-    else{
-      index +=1;
+    } else {
+      index += 1;
     }
   });
 
-  console.log(product)
+  console.log(product);
 }
 ////////////////////////////End Of Cart Products Functionality////////////////////
 
-
 /////////////////////////////////Profile Functionality //////////////////////////
-users = []
+users = [];
 
-function fetchProfiles(){
-  fetch( urlsShowUsers ,{
-    method: 'get',
+function fetchProfiles() {
+  fetch(urlsShowUsers, {
+    method: "get",
     headers: {
-      "Authorization": `jwt ${myStorage.getItem("jwt-token")}`,
-      "Content-Type": "application/json"
+      Authorization: `jwt ${myStorage.getItem("jwt-token")}`,
+      "Content-Type": "application/json",
     },
-})
-.then(res => res.json())
-.then(data => {
-  console.log(data)
-  users = data.data;
-});
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      users = data.data;
+      createProfile(users)
+    });
 }
 fetchProfiles();
 
-
-
-function createProfile(users){
+function createProfile(users) {
   let i = 0;
-    let profilesContainer = document.querySelector("#profile");
-    profilesContainer.innerHTML ="";
-    users.forEach( profile => {
-      profilesContainer.innerHTML += `
+  let profilesContainer = document.querySelector("#profile");
+  profilesContainer.innerHTML = "";
+  users.forEach((profile) => {
+    profilesContainer.innerHTML += `
           <div class="profiles-container">
               <div class="fullnames">
                   <span class="name">${profile[1]}</span>
@@ -313,20 +359,18 @@ function createProfile(users){
                   <span class="login-detail username">${profile[3]}</span>
                   <span class="login-details password">${profile[4]}</span>
               </div>
-              <span class="email">${profiles[5]}</span>
+              <span class="email">${profile[5]}</span>
         </div>
         <div class="buttons button">
           <button class="button" onclick="updateUser()">Update</button>
           <button class="button" onclick="deleteUser()">Delete</button> 
         </div>
       `;
-      i += 1;
-    });
+    i += 1;
+  });
 }
 
-function deleteProfile(){
-
-}
+function deleteProfile() {}
 
 //////////////////////////ADDITIONAL FUNCTIONS////////////////////////
 function showContent(contentName) {
